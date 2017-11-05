56283d2f355644db4bf534b090b8e0ae8f599ba1
Souvik Basu <souvikbasu@gmail.com>
2017-11-05 18:02:50 +0530

    add all fields in food type

--- src/models/food.ts
+++ src/models/food.ts
@@ -1,5 +1,17 @@
 
 export class Food {
   public name: string;
+  public quantity: number;
+  public unit: string;
   public fatInGrams: number;
+  public carbInGm: number;
+  public fiberInGm: number;
+  public netCarbsInGm: number;
+  public proteinInGm: number;
+  public alcoholInGm: number;
+  public type: string;
+  public category: string;
+  public calories: number;
+  public author: string;
+  public restaurant: string;
 }
