db0ae405324ffe4dd451af04ebea30cae220f8c5
Souvik Basu <souvikbasu@gmail.com>
2017-11-06 08:45:58 +0530

    add toolbar and card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import {NgModule} from '@angular/core';
 import {HttpModule, JsonpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule, MatCardModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
-import {FoodService} from "../services/food.service";
+import {FoodService} from '../services/food.service';
 import { FoodItemComponent } from '../components/food-item/food-item.component';
 
 @NgModule({
@@ -20,7 +20,7 @@ import { FoodItemComponent } from '../components/food-item/food-item.component';
     HttpModule,
     JsonpModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule
+    MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule, MatCardModule
   ],
   providers: [FoodService],
   bootstrap: [AppComponent]
